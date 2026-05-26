9bdefe59349470d9b0d1cfba52c46ffc55f4a04b
christophevg-agent <contact+agent@christophe.vg>
2026-05-26 11:44:53 +0200

    chore: bump version to 0.4.0

--- src/yoker/__init__.py
+++ src/yoker/__init__.py
@@ -50,7 +50,7 @@ from yoker.exceptions import (
 from yoker.logging import LoggingContext, configure_logging, get_logger, log_timing
 from yoker.thinking import ThinkingMode
 
-__version__ = "0.3.1"
+__version__ = "0.4.0"
 __author__ = "Christophe VG"
 
 __all__ = [
